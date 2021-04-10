be7184847c2b055c035681f91ebcdbea7c3ef0a8
Brian Elinsky <be2112@gmail.com>
2021-04-10 13:28:57 -0500

    Crawler extracts data from all reviews on the site

--- coffee_ratings_web_crawler/coffee_ratings/spiders/coffee_ratings_spider.py
+++ coffee_ratings_web_crawler/coffee_ratings/spiders/coffee_ratings_spider.py
@@ -7,8 +7,7 @@ class CoffeeRatingsSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.coffeereview.com/review/panama-boquete-flor-de-mariposa-geisha-espresso/',
-            'https://www.coffeereview.com/review/yemen-al-mashtal-al-burhani/'
+            'https://www.coffeereview.com/review/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -24,46 +23,71 @@ class CoffeeRatingsSpider(scrapy.Spider):
                 f.write(response.body)
             self.log(f'Saved file {filename}')
 
+        def extract_if_exists(xpath: str):
+            if response.xpath(xpath):
+                return response.xpath(xpath).get()
+            else:
+                return 'NA'
+
         def parse_review_page(response) -> dict:
             # Parse content
             scraped_data = {}
-            scraped_data['roaster'] = response.xpath('//*[@id="genesis-content"]/article/div/div/div[1]/div[2]/p/text()').get()
-            scraped_data['bean'] = response.xpath('//*[@id="genesis-content"]/article/div/div/div[1]/div[2]/h1/text()').get()
-            scraped_data['rating'] = response.xpath('//*[@id="genesis-content"]/article/div/div/div[1]/div[1]/span/text()').get()
 
-            table = response.xpath('//*[@id="genesis-content"]/article/div/div/div[2]/div[1]/table')
-            scraped_data['roaster_location'] = extract_table_field(table, 0)
-            scraped_data['coffee_origin'] = extract_table_field(table, 1)
-            scraped_data['roast_level'] = extract_table_field(table, 2)
-            scraped_data['agtron'] = extract_table_field(table, 3)
-            scraped_data['estimated_price'] = extract_table_field(table, 4)
-            scraped_data['review_date'] = extract_table_field(table, 5)
-            scraped_data['aroma'] = extract_table_field(table, 6)
-            scraped_data['acidity_structure'] = extract_table_field(table, 7)
-            scraped_data['body'] = extract_table_field(table, 8)
-            scraped_data['flavor'] = extract_table_field(table, 9)
-            scraped_data['aftertaste'] = extract_table_field(table, 10)
+            scraped_data['roaster'] = extract_if_exists('//*[@class=\"review-roaster\"]//text()')
+            scraped_data['bean'] = extract_if_exists('//*[@class=\"review-title\"]//text()')
+            scraped_data['rating'] = extract_if_exists('//*[@class=\"review-template-rating\"]//text()')
 
-            scraped_data['blind_assessment'] = response.xpath('//*[@id="genesis-content"]/article/div/div/p[1]/text()').get()
-            scraped_data['notes'] = response.xpath('//*[@id="genesis-content"]/article/div/div/p[2]/span[1]/text()').get()
-            scraped_data['bottom_line'] = response.xpath('//*[@id="genesis-content"]/article/div/div/p[3]/text()').get()
+            scraped_data['roaster_location'] = extract_if_exists('//*[text()[contains(.,"Roaster Location")]]/following-sibling::td//text()')
+            scraped_data['coffee_origin'] = extract_if_exists('//*[text()[contains(.,"Coffee Origin")]]/following-sibling::td//text()')
+            scraped_data['roast_level'] = extract_if_exists('//*[text()[contains(.,"Roast Level")]]/following-sibling::td//text()')
+            scraped_data['agtron'] = extract_if_exists('//*[text()[contains(.,"Agtron")]]/following-sibling::td//text()')
+            scraped_data['estimated_price'] = extract_if_exists('//*[text()[contains(.,"Est. Price")]]/following-sibling::td//text()')
+            scraped_data['review_date'] = extract_if_exists('//*[text()[contains(.,"Review Date")]]/following-sibling::td//text()')
+            scraped_data['aroma'] = extract_if_exists('//*[text()[contains(.,"Aroma")]]/following-sibling::td//text()')
+            scraped_data['acidity_structure'] = extract_if_exists('//*[text()[contains(.,"Acidity") and contains(.,"Structure")]]/following-sibling::td//text()')
+
+            scraped_data['body'] = extract_if_exists('//*[text()[contains(.,"Body")]]/following-sibling::td//text()')
+            scraped_data['flavor'] = extract_if_exists('//*[text()[contains(.,"Flavor")]]/following-sibling::td//text()')
+            scraped_data['aftertaste'] = extract_if_exists('//*[text()[contains(.,"Aftertaste")]]/following-sibling::td//text()')
+            scraped_data['with_milk'] = extract_if_exists('//*[text()[contains(.,"With Milk")]]/following-sibling::td//text()')
+
+
+            scraped_data['blind_assessment'] = extract_if_exists('//*[text()[contains(.,"Blind Assessment")]]/following-sibling::text()')
+            scraped_data['notes'] = extract_if_exists('//*[text()[contains(.,"Notes")]]/following-sibling::text()')
+            scraped_data['bottom_line'] = extract_if_exists('//*[text()[contains(.,"The Bottom Line")]]/following-sibling::text()')
+            scraped_data['who_should_drink_it'] = extract_if_exists('//*[text()[contains(.,"Who Should Drink It")]]/following-sibling::text()')
 
             return scraped_data
 
+        def extract_coffee_review_urls(response) -> list:
+            """Given a url that has a list of reviews (e.g. the latest reviews webpage), crawl the page, parse urls to
+            other review pages, and return the list of those urls.
+            """
+            return response.xpath('//a[@title="Read Complete Review"]/@href').getall()
+
+        def extract_next_page_url(response) -> list:
+            """Given a url that has a list of reviews, crawl the page, return the url to teh next page of reviews."""
+            if response.xpath('//li[@class="pagination-next"]//a/@href'):
+                return [response.xpath('//li[@class="pagination-next"]//a/@href').get()]
+            else:
+                return []
+
         page_dirname = 'raw_coffee_data'
+        self.log("parsing page: " + response.url)
 
-        save_raw_data(response, page_dirname)
+        urls_to_crawl = extract_coffee_review_urls(response)
+        urls_to_crawl += extract_next_page_url(response)
 
-        scraped_data = parse_review_page(response)
+        self.log("URLs scraped to review still " + str(urls_to_crawl))
+        if urls_to_crawl:
+            for url in urls_to_crawl:
+                yield scrapy.Request(url=url, callback=self.parse)
+        else:
+            save_raw_data(response, page_dirname)
+            self.log("parse a single review for: " + str(response.url))
+            scraped_data = parse_review_page(response)
+            yield scraped_data
 
-        yield scraped_data
 
-
-# TODO - I need to figure out how to crawl the website
-# High level plan - start the extract on the latest reviews page: https://www.coffeereview.com/review/
-# Then extract that page
-# Write a function that tells you if a page is a review page.
-# If review page, then extract and terminate
-# If not review page, then look for links to reviews (maybe look for all 'read complete reviews' links
-# I also need to change pages.
-# TODO - I need to handle the case where a review does not have all of the field that I want.
+# TODO - add the URL to the parsed JSON document
+# TODO - add a try catch to the parse, log when you get an error parsing.
